15e10004984ab082ffee1495430b5f1b460a32e3
Jim <jympire@gmail.com>
2018-07-18 16:31:16 -0700

    update

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,46 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def line_of_credit
+  end
+
+  def invoice_factoring
+  end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def faq
+  end
+
+  def client_stories
+  end
+
+  def careers
+  end
+
+  def partner
+  end
+
+  def press
+  end
+
+  def pricing
+  end
+
+  def how_it_works
+  end
+
+  def support
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,33 @@
+Rails.application.routes.draw do
+  
+  root 'pages#home'
+
+  get 'line-of-credit' , to: 'pages#line_of_credit'
+
+  get 'invoice-factoring' , to: 'pages#invoice_factoring'
+
+  get 'about' , to: 'pages#about'
+
+  get 'blog' , to: 'pages#blog'
+
+  get 'faq' , to: 'pages#faq'
+
+  get 'client-stories' , to: 'pages#client_stories'
+
+  get 'careers' , to: 'pages#careers'
+
+  get 'partner' , to: 'pages#partner'
+
+  get 'press' , to: 'pages#press'
+
+  get 'pricing' , to: 'pages#pricing'
+
+  get 'how-it-works' , to: 'pages#how_it_works'
+
+  get 'support' , to: 'pages#support'
+
+  get 'privacy' , to: 'pages#privacy'
+
+  get 'terms' , to: 'pages#terms'
+
+end
